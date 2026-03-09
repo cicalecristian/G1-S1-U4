5a3cc2ad9fdfbc34db1df50df78756cecbd24a6b
cicalecristian <cicalecristian@gmail.com>
2026-03-09 15:20:05 +0100

    first ex.

--- src/Es1.java
+++ src/Es1.java
@@ -1,6 +1,51 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Es1 {
-    static void main() {
-        System.out.println("ciao");
-        System.out.println("oi");
+//    public static int moltiplica(int a, int b) {
+//        return a * b;
+//    }
+
+//    public static String concat(String a, int b) {
+//        return a + b;
+//    }
+
+//    public static String[] inserisciAlTerzo(String[] arr, String nuova) {
+//
+//        String[] nuovoArray = new String[6];
+//
+//        nuovoArray[0] = arr[0];
+//        nuovoArray[1] = arr[1];
+//
+//        nuovoArray[2] = nuova;
+//
+//        nuovoArray[3] = arr[2];
+//        nuovoArray[4] = arr[3];
+//        nuovoArray[5] = arr[4];
+//
+//        return nuovoArray;
+//    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+//        System.out.println("inserisci il primo numero: ");
+//        int firstNumber = Integer.parseInt(scanner.nextLine());
+//        System.out.println("inserisci il secondo numero: ");
+//        int secondNumber = Integer.parseInt(scanner.nextLine());
+//        int risultato = moltiplica(firstNumber, secondNumber);
+//        System.out.println("Il prodotto è: " + risultato);
+
+//        System.out.println("inserisci il primo valore: ");
+//        String firstValue = scanner.nextLine();
+//        System.out.println("inserisci il secondo valore: ");
+//        int secondValue = Integer.parseInt(scanner.nextLine());
+//        String result = concat(firstValue, secondValue);
+//        System.out.println("Il risultato è: " + result);
+
+//        String[] originale = {"A", "B", "C", "D", "E"};
+//        String nuovaStringa = "X";
+//
+//        String[] risultato = inserisciAlTerzo(originale, nuovaStringa);
+//        System.out.println(Arrays.toString(risultato));
     }
 }
