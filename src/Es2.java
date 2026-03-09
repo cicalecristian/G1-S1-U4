996e2ce0b04723a15c57eebd792dc70f94d2162c
cicalecristian <cicalecristian@gmail.com>
2026-03-09 15:38:44 +0100

    second ex.

--- src/Es2.java
+++ src/Es2.java
@@ -1,5 +1,22 @@
-public class Es2 {
-    static void main() {
+import java.util.Scanner;
 
+public class Es2 {
+    static String concat(String prima, String seconda, String terza){
+        return prima + " " + seconda + " " + terza;
+    }
+
+    static void main() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("inserisci la prima stringa: ");
+        String firstString = scanner.nextLine();
+        System.out.println("inserisci la seconda stringa: ");
+        String secondString = scanner.nextLine();
+        System.out.println("inserisci la terza stringa: ");
+        String thirdString = scanner.nextLine();
+        System.out.println();
+        String concatenazione = concat(firstString, secondString, thirdString);
+        String invertita = concat(thirdString, secondString, firstString);
+        System.out.println(concatenazione);
+        System.out.println(invertita);
     }
 }
